b956bee80754271d13366e7d043266ef26a2c7ac
Joshua Becnel <jcbecnel@gmail.com>
2024-09-12 12:45:17 -0500

    improved street name accuracy

--- src/parse/street.rs
+++ src/parse/street.rs
@@ -1,4 +1,4 @@
-use crate::{regex::{regex_map, REG_STREET_NUMBER}, Address};
+use crate::{components::street_type::{STREET_TYPE, STREET_TYPE_ABBVR}, regex::{regex_map, REG_STREET_NUMBER, REG_STRIP_STREET0, REG_STRIP_STREET1, REG_STRIP_STREET2, REG_STRIP_STREET3, REG_STRIP_STREET_ABBVR0, REG_STRIP_STREET_ABBVR1, REG_STRIP_STREET_ABBVR2, REG_STRIP_STREET_ABBVR3}, Address};
 
 pub fn parse_street_no(mut address: Address, full_address: &str) -> Address {
     address.street_no = regex_map(full_address, &REG_STREET_NUMBER);
@@ -18,17 +18,34 @@ pub fn parse_street_name(mut address: Address, full_address: &str) -> Address {
         None => street_name = street_name,
     }
 
-    match &address.street_type {
-        Some(val) => street_name = street_name.replace(&format!(" {} ", val), " "),
-        None => street_name = street_name,
-    }
-
     match &address.unit_type {
         Some(val) => {
             street_name = street_name.replace(&format!(" {} ", val), " ");
             match &address.unit_no {
                 Some(val) => street_name = street_name.replace(&format!(" {} ", val), " "),
                 None => street_name = street_name,
+            }
+        },
+        None => street_name = street_name,
+    }
+    
+    match &address.street_type {
+        Some(val) => {
+            let size = STREET_TYPE_ABBVR.len() / 4;
+            match val {
+                v if STREET_TYPE_ABBVR[..size].contains(&v.to_lowercase().as_str()) => street_name = REG_STRIP_STREET_ABBVR0.replace(&street_name, "").to_string(),
+                v if STREET_TYPE_ABBVR[size..size*2].contains(&v.to_lowercase().as_str()) => street_name = REG_STRIP_STREET_ABBVR1.replace(&street_name, "").to_string(),
+                v if STREET_TYPE_ABBVR[size*2..size*3].contains(&v.to_lowercase().as_str()) => street_name = REG_STRIP_STREET_ABBVR2.replace(&street_name, "").to_string(),
+                v if STREET_TYPE_ABBVR[size*3..].contains(&v.to_lowercase().as_str()) => street_name = REG_STRIP_STREET_ABBVR3.replace(&street_name, "").to_string(),
+                val => {
+                    match val {
+                        v if STREET_TYPE[..size].contains(&v.to_lowercase().as_str()) => street_name = REG_STRIP_STREET0.replace(&street_name, "").to_string(),
+                        v if STREET_TYPE[size..size*2].contains(&v.to_lowercase().as_str()) => street_name = REG_STRIP_STREET1.replace(&street_name, "").to_string(),
+                        v if STREET_TYPE[size*2..size*3].contains(&v.to_lowercase().as_str()) => street_name = REG_STRIP_STREET2.replace(&street_name, "").to_string(),
+                        v if STREET_TYPE[size*3..].contains(&v.to_lowercase().as_str()) => street_name = REG_STRIP_STREET3.replace(&street_name, "").to_string(),
+                        _ => street_name = street_name,
+                    }
+                },
             }
         },
         None => street_name = street_name,
--- src/regex.rs
+++ src/regex.rs
@@ -120,3 +120,68 @@ pub static REG_UNIT: Lazy<Regex> = Lazy::new(||{
     }).collect::<Vec<String>>();
     Regex::new(&(&modified.join("|"))).unwrap()
 });
+
+// ============================
+pub static REG_STRIP_STREET_ABBVR0: Lazy<Regex> = Lazy::new(||{
+    let size = STREET_TYPE_ABBVR.len() / 4;
+    let modified = STREET_TYPE_ABBVR[..size].iter().map(|street| {
+        format!(r"(?:^|\W){}(?:$|\W)$", street.to_ascii_uppercase())
+    }).collect::<Vec<String>>();
+    Regex::new(&(&modified.join("|"))).unwrap()
+});
+
+pub static REG_STRIP_STREET_ABBVR1: Lazy<Regex> = Lazy::new(||{
+    let size = STREET_TYPE_ABBVR.len() / 4;
+    let modified = STREET_TYPE_ABBVR[size..size*2].iter().map(|street| {
+        format!(r"(?:^|\W){}(?:$|\W)$", street.to_ascii_uppercase())
+    }).collect::<Vec<String>>();
+    Regex::new(&(&modified.join("|"))).unwrap()
+});
+
+pub static REG_STRIP_STREET_ABBVR2: Lazy<Regex> = Lazy::new(||{
+    let size = STREET_TYPE_ABBVR.len() / 4;
+    let modified = STREET_TYPE_ABBVR[size*2..size*3].iter().map(|street| {
+        format!(r"(?:^|\W){}(?:$|\W)$", street.to_ascii_uppercase())
+    }).collect::<Vec<String>>();
+    Regex::new(&(&modified.join("|"))).unwrap()
+});
+
+pub static REG_STRIP_STREET_ABBVR3: Lazy<Regex> = Lazy::new(||{
+    let size = STREET_TYPE_ABBVR.len() / 4;
+    let modified = STREET_TYPE_ABBVR[size*3..].iter().map(|street| {
+        format!(r"(?:^|\W){}(?:$|\W)$", street.to_ascii_uppercase())
+    }).collect::<Vec<String>>();
+    Regex::new(&(&modified.join("|"))).unwrap()
+});
+
+pub static REG_STRIP_STREET0: Lazy<Regex> = Lazy::new(||{
+    let size = STREET_TYPE.len() / 4;
+    let modified = STREET_TYPE[..size].iter().map(|street| {
+        format!(r"(?:^|\W){}(?:$|\W)$", street.to_ascii_uppercase())
+    }).collect::<Vec<String>>();
+    Regex::new(&(&modified.join("|"))).unwrap()
+});
+
+pub static REG_STRIP_STREET1: Lazy<Regex> = Lazy::new(||{
+    let size = STREET_TYPE.len() / 4;
+    let modified = STREET_TYPE[size..size*2].iter().map(|street| {
+        format!(r"(?:^|\W){}(?:$|\W)$", street.to_ascii_uppercase())
+    }).collect::<Vec<String>>();
+    Regex::new(&(&modified.join("|"))).unwrap()
+});
+
+pub static REG_STRIP_STREET2: Lazy<Regex> = Lazy::new(||{
+    let size = STREET_TYPE.len() / 4;
+    let modified = STREET_TYPE[size*2..size*3].iter().map(|street| {
+        format!(r"(?:^|\W){}(?:$|\W)$", street.to_ascii_uppercase())
+    }).collect::<Vec<String>>();
+    Regex::new(&(&modified.join("|"))).unwrap()
+});
+
+pub static REG_STRIP_STREET3: Lazy<Regex> = Lazy::new(||{
+    let size = STREET_TYPE.len() / 4;
+    let modified = STREET_TYPE[size*3..].iter().map(|street| {
+        format!(r"(?:^|\W){}(?:$|\W)$", street.to_ascii_uppercase())
+    }).collect::<Vec<String>>();
+    Regex::new(&(&modified.join("|"))).unwrap()
+});
